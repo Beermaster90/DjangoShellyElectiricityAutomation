796a2bc97bcd928d30a295ebb6565c0d7b07978f
Eal <Eal@DESKTOP-9CSRF50>
2025-03-12 19:14:27 +0200

    Fixed current status of the device

--- DjangoShellyElectiricityAutomation/app/urls.py
+++ DjangoShellyElectiricityAutomation/app/urls.py
@@ -9,7 +9,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index'),  # /shellyapp/ calls index()
 
-    path('fetch-device-status/', fetch_device_status, name='fetch_device_status')#,  # Specific shelly view
-    # path('toggle-device-output/', toggle_device_output, name='toggle_device_output'),  # page to toggle shelly device output on / off]
-    # path('fetch-prices/', call_fetch_prices, name='fetch_prices'),  # fetch electricity prices
+    path('fetch-device-status/', fetch_device_status, name='fetch_device_status'),  # Specific shelly view
+    path('toggle-device-output/', toggle_device_output, name='toggle_device_output'),  # page to toggle shelly device output on / off]
+    path('fetch-prices/', call_fetch_prices, name='fetch_prices'),  # fetch electricity prices
 ]
